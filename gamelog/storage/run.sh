19451d9737e08de37484aab5989999a4c64924a5
Nichole <123576128@qq.com>
2015-03-18 18:05:29 +0800

    no message

--- gamelog/storage/run.sh
+++ gamelog/storage/run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/
+nohup ./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/ &./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/
--- test/test.go
+++ test/test.go
@@ -3,36 +3,91 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sync"
 )
 
-func splitInit(s rune) bool {
-	if s == ' ' || s == '"' {
+type Set struct {
+	m map[int]bool
+	sync.RWMutex
+}
+
+func New() *Set {
+	return &Set{
+		m: map[int]bool{},
+	}
+}
+
+func (s *Set) Add(item int) {
+	s.Lock()
+	defer s.Unlock()
+	s.m[item] = true
+}
+
+func (s *Set) Remove(item int) {
+	s.Lock()
+	s.Unlock()
+	delete(s.m, item)
+}
+
+func (s *Set) Has(item int) bool {
+	s.RLock()
+	defer s.RUnlock()
+	_, ok := s.m[item]
+	return ok
+}
+
+func (s *Set) Len() int {
+	return len(s.List())
+}
+
+func (s *Set) Clear() {
+	s.Lock()
+	defer s.Unlock()
+	s.m = map[int]bool{}
+}
+
+func (s *Set) IsEmpty() bool {
+	if s.Len() == 0 {
 		return true
 	}
 	return false
 }
 
-func splitUnderline(s rune) bool {
-	if s == '_' {
-		return true
+func (s *Set) List() []int {
+	s.RLock()
+	defer s.RUnlock()
+	list := []int{}
+	for item := range s.m {
+		list = append(list, item)
 	}
-	return false
+	return list
 }
 
 func main() {
-	str := "DEPOSIT_ITEMS ({\"100313130_5_8\",\"100102496_8_8\",\"100101132_8_8\",\"0_0_0\",})"
+	// 初始化
+	s := New()
 
-	ret := strings.FieldsFunc(str, splitInit)
+	s.Add(1)
+	s.Add(1)
+	s.Add(2)
 
-	for _, v := range ret {
-		if strings.Count(v, "_") == 2 {
-			under := strings.FieldsFunc(v, splitUnderline)
-			fmt.Println(under)
-			fmt.Println(v)
-		}
+	s.Clear()
+	if s.IsEmpty() {
+		fmt.Println("0 item")
 	}
 
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+	s.Add(2)
+
+	if s.Has(2) {
+		fmt.Println("2 does exist")
+	}
+
+	s.Remove(2)
+	s.Remove(3)
+	fmt.Println("list of all items", s.List())
 }
 
 //func main() {
