c902e4d6b455c482d76d5839666adcfdc5d8069b
Nichole <123576128@qq.com>
2015-03-18 17:54:24 +0800

    更新文件

--- gamelog/storage/make.sh
+++ gamelog/storage/make.sh
@@ -2,4 +2,4 @@
 
 go build -i
 
-#scp -r storage/ root@10.100.0.181:scp -r /root/go/src/github.com/NicholeGit/work/gamelog/storage root@10.100.0.181:/home/biefu/nichole/
+#scp -r /root/go/src/github.com/NicholeGit/work/gamelog/storage root@10.100.0.181:/home/biefu/nichole/
