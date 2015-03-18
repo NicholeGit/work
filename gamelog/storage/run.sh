7805d82832d1454dd823afdbc75e8fac30115d6d
Nichole <123576128@qq.com>
2015-03-18 18:07:57 +0800

    no message

--- gamelog/storage/run.sh
+++ gamelog/storage/run.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
+rm  -f  out_*.log
 nohup ./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/ &./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/
