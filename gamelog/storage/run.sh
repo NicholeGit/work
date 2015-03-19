23b456cb1a2a2d84d955add600fce3c1606f1dfb
Nichole <123576128@qq.com>
2015-03-19 16:18:41 +0800

    更新运行shell

--- gamelog/storage/run.sh
+++ gamelog/storage/run.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 
 rm  -f  out_*.log
-nohup ./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/ &./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/
+killall storage
+nohup ./storage -file /home/biefu/rsync_insertsql_20150311/djs/upinfo.tmp -path /home/biefu/serverdata/lzservers/116.213.193.35/M9301/userdata/ &
