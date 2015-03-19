9e17f77bd0d59790af5c272f9dd5f3a0a0b7b984
Nichole <123576128@qq.com>
2015-03-19 17:32:44 +0800

    no message

--- gamelog/storage/query.sh
+++ gamelog/storage/query.sh
@@ -3,15 +3,6 @@ data=`date +%Y%m%d`
 
 dir=./all_$data
 
-diruser=./user_cangku_fz_$data
-diruser1=./user_cangku_sx_$data
-diruser2=./user_cangku_zhenbao_$data
-diruser3=./user_cangku_fujia_$data
-fz_config=./fz_setup.ini
-sx_config=./sx_setup.ini
-zb_config=./zb_setup.ini
-fj_config=./fj_setup.ini
-
 username="sgcuser"
 password="gamlaxy"
 db="testStorage"
@@ -25,12 +16,12 @@ rm -f $dir
 
 ret=`mysql -u$username -p$password $db -e "SELECT objectId,count(id) FROM storage group by objectId"`
 
-echo $ret|awk '{ i = 3; while ( i <= NF ) { print $i"\t"$(i+1); i+=2}}' >> $dir 
+echo $ret|awk '{ i = 3; while ( i <= NF ) { print $i"\t"$(i+1); i+=2}}' >> $dir
 
 
 #cat $fz_config|while read line
-#do        
-#        echo -ne $line"\t" >>$diruser 
+#do
+#        echo -ne $line"\t" >>$diruser
 #done
 #echo -ne "\n" >>$diruser
 #cat $fz_config|while read line
@@ -38,6 +29,6 @@ echo $ret|awk '{ i = 3; while ( i <= NF ) { print $i"\t"$(i+1); i+=2}}' >> $dir
 #	ret=`mysql -u$username -p$password $db -e "SELECT count(id) FROM storage where objectid=$line"`
 #	cnt=`echo $ret|awk '{print $2}'`
 #	sum=`mysql -u$username -p$password $db -e "SELECT sum(count) FROM storage where objectid=$line"`
-#	echo -ne $cnt"\t" >>$diruser 
+#	echo -ne $cnt"\t" >>$diruser
 #done
 
